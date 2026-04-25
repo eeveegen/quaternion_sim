a7da91c22de80f8fcafb3cbeeb7181f1e6375839
Iveta <iveta.gencheva@rwth-aachen.de>
2026-04-25 23:01:24 +0200

    added double-sided death detection

--- blender_ext_intf.py
+++ blender_ext_intf.py
@@ -1,16 +1,50 @@
 import socket
 import time
+import threading
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+DATA_PORT = 5005
+HB_PORT = 5006 # back connection -> listen for heartbeat
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+BLENDER_TIMEOUT = 7 # seconds
+timeout = False
 
-for i in range(15):
-    msg = "Running " + str(i)
-    print(msg)
-    sock.sendto(msg.encode(), (UDP_IP, UDP_PORT))
-    time.sleep(1)
+def watchdog():
+    global timeout
+    hb_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    hb_sock.bind((UDP_IP, HB_PORT))
+    hb_sock.settimeout(BLENDER_TIMEOUT)
 
-print("Completed!")
+    while True:
+        try:
+            hb_sock.recvfrom(1024)
+        except socket.timeout:
+            timeout = True
+            break
+    
+    hb_sock.close()
+    print("Blender process timed out. Terminating.")
+
+def fake_send():
+    data_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    i = 0
+
+    while True:
+        msg = "Running " + str(i)
+        print(msg)
+        data_sock.sendto(msg.encode(), (UDP_IP, DATA_PORT))
+        i = i + 1
+        time.sleep(1)
+
+        if i > 15:
+            break
+        if timeout:
+            break
+    
+    data_sock.close()
+    print("Demo finished.")
+
+    
+threading.Thread(target=watchdog, daemon=True).start()
+threading.Thread(target=fake_send).start()
 
--- blender_int_ctrl.py
+++ blender_int_ctrl.py
@@ -3,16 +3,22 @@ import bpy
 import socket
 import threading
 
+DATA_TIMEOUT = 5
+
 print("Start")
 
 def udp_listener():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("127.0.0.1", 5005))
-    sock.settimeout(5.0)
+    sock.settimeout(DATA_TIMEOUT)
+    
+    ack_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     while True:    
         try:
             data, _ = sock.recvfrom(1024)
+            ack_sock.sendto("ACK".encode(), ("127.0.0.1", 5006))
+            
             msg = data.decode().strip()
             print(msg)
         except socket.timeout:
